4fff3cb0b4263bad7485b639ebb22adcea307777
joaquincernik <joaquincernik@gmail.com>
2025-05-06 10:10:35 -0300

    modificado de la estructura del proyecto

--- app/src/main/java/com/undef/manosLocalesCernikGaribaldi/activities/MainActivity.kt
+++ app/src/main/java/com/undef/manosLocalesCernikGaribaldi/activities/MainActivity.kt
@@ -1,12 +1,8 @@
-package com.undef.manosLocalesCernikGaribaldi
+package com.undef.manosLocalesCernikGaribaldi.activities
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
