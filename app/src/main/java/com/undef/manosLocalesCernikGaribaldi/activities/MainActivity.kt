6c0571474232d4387ff9d7300dc704819aa71b73
bruno <bruno_garibaldi12@hotmail.com>
2025-05-07 03:34:29 -0300

    registro de usuario fixed y selecciona tu provincia fixed

--- app/src/main/java/com/undef/manosLocalesCernikGaribaldi/activities/MainActivity.kt
+++ app/src/main/java/com/undef/manosLocalesCernikGaribaldi/activities/MainActivity.kt
@@ -1,61 +1,76 @@
 package com.undef.manosLocalesCernikGaribaldi.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.undef.manosLocalesCernikGaribaldi.R
+import com.undef.manosLocalesCernikGaribaldi.databinding.ActivityMainBinding
 import com.undef.manosLocalesCernikGaribaldi.ui.theme.ManosLocalesTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ManosLocalesTheme {
-                FirstText(name = "Joaquin")
-            }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnIrARegistro.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
-
-       /* enableEdgeToEdge()
-        setContent {
-            ManosLocalesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }*/
     }
 }
+//            ManosLocalesTheme {
+//                FirstText(name = "Joaquin")
+//            }
+//        }
+//
+//       /* enableEdgeToEdge()
+//        setContent {
+//            ManosLocalesTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }*/
 
-@Composable
-fun FirstText(name: String){
-    Text(
-        text = "Hola $name"
-    )
-}
-@Composable
-@Preview
-fun FirstTextPreview(){
-    FirstText("Joaquin")
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ManosLocalesTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun FirstText(name: String){
+//    Text(
+//        text = "Hola $name"
+//    )
+//}
+//@Composable
+//@Preview
+//fun FirstTextPreview(){
+//    FirstText("Joaquin")
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ManosLocalesTheme {
+//        Greeting("Android")
+//    }
+//}
--- app/src/main/java/com/undef/manosLocalesCernikGaribaldi/activities/SignUpActivity.kt
+++ app/src/main/java/com/undef/manosLocalesCernikGaribaldi/activities/SignUpActivity.kt
@@ -2,17 +2,39 @@ package com.undef.manosLocalesCernikGaribaldi.activities
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.undef.manosLocalesCernikGaribaldi.R
+import com.undef.manosLocalesCernikGaribaldi.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        // Inicializamos ViewBinding
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Cargamos las provincias desde los recursos
+        val provincias = resources.getStringArray(R.array.provincias_array)
+
+        // Creamos el adaptador para el AutoCompleteTextView
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, provincias)
+
+        // Configuramos el AutoCompleteTextView
+        binding.autoProvincia.setAdapter(adapter)
+        binding.autoProvincia.threshold = 1
+
+
+
+
 
         }
 }
