f13315534781e4a585597d6a1ffec1e3a9f1f3e5
bruno <bruno_garibaldi12@hotmail.com>
2025-05-08 02:43:52 -0300

    signUp funcionando

--- app/src/main/java/com/undef/manosLocalesCernikGaribaldi/activities/SignUpActivity.kt
+++ app/src/main/java/com/undef/manosLocalesCernikGaribaldi/activities/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.undef.manosLocalesCernikGaribaldi.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,6 +9,7 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,15 +19,29 @@ import com.undef.manosLocalesCernikGaribaldi.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
 
-    //todo esto es para lo de las provincias
+
+
+    var mail_check = false
+    var password_check = false
+    var provincias_check = false
+    var usuario_check = false
     private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
+        //todo esto es para lo de las provincias
         // Inicializamos ViewBinding
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Boton para volver atras ARREGLAR ACA SOLO VUELVE A LA MAIN ACTIVITY
+        binding.arrowbacksignup.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
         // Cargamos las provincias desde los recursos
         val provincias = resources.getStringArray(R.array.provincias_array)
@@ -55,6 +71,7 @@ class SignUpActivity : AppCompatActivity() {
          *
          *
          * **/
+        //faltaria que checkee si no existe en la bd registrada
         binding.inputMail.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 val email = s.toString() //obtemenos el texto del editText
@@ -66,11 +83,13 @@ class SignUpActivity : AppCompatActivity() {
                     binding.statusMailCheck.visibility = View.VISIBLE //acordate que los ids son "@+id/status_mail_check"
                     binding.statusMailError.visibility = View.GONE
                     binding.textoErrorMail.visibility = View.GONE
+                    mail_check = true
                 } else {
                     // Email inválido
                     binding.statusMailCheck.visibility = View.GONE
                     binding.statusMailError.visibility = View.VISIBLE
                     binding.textoErrorMail.visibility = View.VISIBLE
+                    mail_check = false
                 }
             }
 
@@ -89,11 +108,13 @@ class SignUpActivity : AppCompatActivity() {
                     binding.statusPasswordCheck.visibility = View.VISIBLE
                     binding.statusPasswordError.visibility = View.GONE
                     binding.textoErrorPassword.visibility = View.GONE
+                    password_check = true
 
                 } else{
                     binding.statusPasswordCheck.visibility = View.GONE
                     binding.statusPasswordError.visibility = View.VISIBLE
                     binding.textoErrorPassword.visibility = View.VISIBLE
+                    password_check = false
                 }
 
             }
@@ -102,13 +123,78 @@ class SignUpActivity : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
 
-        // checkeo de usuario existencia en BD
-        /** TODO **/
+        // checkeo de usuario existencia en BD y que no este vacio
+        binding.inputUsuario.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                var input = s.toString()
+
+                if (input.length > 0){ //solo corrobora que el usuario tenga un nombre mayor a cero, despues falta verificar que no pueda haber duplciados
+                    binding.statusUsuarioCheck.visibility = View.VISIBLE
+                    binding.statusUsuarioError.visibility = View.GONE
+                    binding.textoErrorUsuario.visibility = View.GONE
+                    usuario_check = true
+                } else {
+                    binding.statusUsuarioCheck.visibility = View.GONE
+                    binding.statusUsuarioError.visibility = View.VISIBLE
+                    binding.textoErrorUsuario.visibility = View.VISIBLE
+                    usuario_check = false
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+
 
         //checkeo de provincias
+        binding.autoProvincia.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val inputProvincia = s.toString().trim().lowercase() //esto lo ingresa el usuario
+                val provincias = resources.getStringArray(R.array.provincias_array).map { it.lowercase() } //acordate que map recorre todos los elementos del arreglo y le aplica una fun.
+                //aca tendriamos un arreglo de pronvincias validas
+
+                if (provincias.contains(inputProvincia)){ //si esta
+                    binding.statusProvinciaCheck.visibility = View.VISIBLE
+                    binding.statusProvinciaError.visibility = View.GONE
+                    binding.textoErrorProvincia.visibility = View.GONE
+                    provincias_check = true
+                } else { // Provincia inválida
+                    binding.statusProvinciaCheck.visibility = View.GONE
+                    binding.statusProvinciaError.visibility = View.VISIBLE
+                    binding.textoErrorProvincia.visibility = View.VISIBLE
+                    provincias_check = false
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        //checkeo registrarme
+
+        binding.buttonRegistrarme.setOnClickListener {
+            //validamos telefono
+            val telefono = binding.inputTelefono.text.toString().trim()
+            val esValido = Patterns.PHONE.matcher(telefono).matches() && telefono.length >= 8
+
+            //validamos ciudad
+            val ciudad = binding.inputCiudad.text.toString().trim().length > 0
+
+            if(esValido && ciudad && provincias_check && usuario_check && mail_check && password_check){
+                Toast.makeText(this, "Usuario Creado Con Exito", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java) // aca esta volviendo a la main, tendria que avanzar a la siguiente aca
+                startActivity(intent)
+
+            } else{
+                Toast.makeText(this, "Revisar los Campos", Toast.LENGTH_SHORT).show()
+            }
 
 
 
+        }
 
         }
 
